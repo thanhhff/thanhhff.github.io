838c5cc25e1554db167686588cdd66c360065896
thanhhff <thanhnguyen200399@gmail.com>
2025-05-08 01:57:48 +0000

    Deploying to gh-pages from @ thanhhff/thanhhff.github.io@648615fec0164012f513daefeb7a0b922742ada1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -158,6 +158,9 @@ ninja.data = [{
           section: "News",},{id: "news-i-received-an-invitation-from-tsinghua-university-for-the-2025-make-it-shenzhen-program-china",
           title: 'I received an invitation from Tsinghua University for the 2025 MAKE IT SHENZHEN...',
           description: "",
+          section: "News",},{id: "news-i-was-selected-to-participate-in-the-mediterranean-machine-learning-program-croatia",
+          title: 'I was selected to participate in the Mediterranean Machine Learning program, Croatia.',
+          description: "",
           section: "News",},{
         id: 'social-github',
         title: 'GitHub',
